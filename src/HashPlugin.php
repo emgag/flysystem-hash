80e58205c5931a60424ea958d9f3a9a97e815c71
Matthias Blaser <git@mooch.ch>
2015-09-28 21:14:54 +0200

    cleanup

--- src/HashPlugin.php
+++ src/HashPlugin.php
@@ -1,20 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: maetthu
- * Date: 9/28/15
- * Time: 7:13 PM
- */
-
 namespace Emgag\Flysystem\Hash;
 
-
-use League\Flysystem\NotSupportedException;
 use League\Flysystem\Plugin\AbstractPlugin;
 
+/**
+ * Flysystem plugin to hash file contents
+ */
 class HashPlugin extends AbstractPlugin
 {
-
     /**
      * Get the method name.
      *
@@ -28,30 +21,30 @@ class HashPlugin extends AbstractPlugin
     /**
      * Returns hash value of given path using supplied hash algorithm
      *
-     * @param null $path
+     * @param null   $path
      * @param string $algo any algorithm supported by hash()
+     * @return string|bool
      * @see http://php.net/hash
      */
-    public function handle($path = null, $algo = 'sha256'){
-        if(!in_array($algo, hash_algos())){
+    public function handle($path = null, $algo = 'sha256')
+    {
+        if (!in_array($algo, hash_algos())) {
             throw new \InvalidArgumentException('Hash algorithm ' . $algo . ' is not supported');
         }
 
         $stream = $this->filesystem->readStream($path);
 
-        if($stream === false){
+        if ($stream === false) {
             return false;
         }
 
         $hc = hash_init($algo);
 
-        if(hash_update_stream($hc, $stream) === false){
+        if (hash_update_stream($hc, $stream) === false) {
             return false;
         }
 
         return hash_final($hc);
     }
-
-
 
 }
